bdb26d8b722d4605b7f6cd998e08dd09e2ad5adc
school-wuhahah <1589313619@qq.com>
2019-07-11 22:36:21 +0800

    Update : 添加 lua中json解析 lua_cjson 库（未完）

--- CppInterface/CppInterface/LuaTest.cpp
+++ CppInterface/CppInterface/LuaTest.cpp
@@ -1,5 +1,6 @@
 #include "LuaTest.h"
 #include "Debuger.h"
+#include "lua-cjson-2.1.0/lua_cjson.c"
 
 void LuaTest::init_register_luaState(lua_State *L)
 {
@@ -8,6 +9,9 @@ void LuaTest::init_register_luaState(lua_State *L)
 		luaL_openlibs(L);
 		create_test_luafunc(L);
 		lua_get_testglobal_table(L);
+		//---lua_cjson
+		luaopen_cjson(L);
+		luaopen_cjson_safe(L);
 	}
 }
 
